1794a053cff37a014908b44e748e2931e137a3c0
kaleytsangbcit <ktsang38@my.bcit.ca>
2023-03-06 01:04:38 -0800

    add logo

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/logo.png" />
             </Head>
             
             <main className={styles.main}>
